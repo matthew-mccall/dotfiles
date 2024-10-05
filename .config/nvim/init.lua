964a18be373a28332fd2d69cf758e14d5aa058ae
Matthew McCall <m-mccall+git@outlook.com>
2024-10-05 09:34:39 -0400

    Simplified configuration, removed PHP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,7 +8,6 @@ vim.opt.number = true
 vim.opt.breakindent = true
 vim.opt.lbr = true
 
-vim.opt.termguicolors = true
 vim.opt.exrc = true
 
 vim.call('plug#begin')
@@ -103,22 +102,6 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('lspconfig').clangd.setup {
     capabilities = capabilities
-}
-
-vim.filetype.add({
-  pattern = {
-    ['.*%.blade%.php'] = 'blade',
-  },
-})
-
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.blade = {
-  install_info = {
-    url = "https://github.com/EmranMR/tree-sitter-blade",
-    files = {"src/parser.c"},
-    branch = "main",
-  },
-  filetype = "blade"
 }
 
 require('nvim-treesitter.configs').setup { highlight = { enable = true } }
