b0051e18809d1a9fb710724e66763ad9b01cac35
Matthew McCall <m-mccall+git@outlook.com>
2025-04-06 13:03:15 -0400

    Migrate from vim-plug to lazy.nvim

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "matthew-mccall/monokai-pro.nvim",
+        branch = "feature-get-macos-appearance",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require("monokai-pro").setup({
+                day_night = {
+                    enable = true,
+                    day_filter = "light",
+                    night_filter = "spectrum",
+                }
+            })
+            -- load the colorscheme here
+            vim.cmd([[colorscheme monokai-pro]])
+        end,
+    },
+    { "nvim-lualine/lualine.nvim", opts = { options = { icons_enabled = false } }} ,
+    "akinsho/bufferline.nvim"
+}
